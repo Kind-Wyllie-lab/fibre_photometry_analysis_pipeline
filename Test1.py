671193400f83a1af3551156f3a4be68dad4716c0
Lou <l.gineste@sms.ed.ac.uk>
2026-02-24 15:53:53 +0000

    Add function to save the cleaned version of the fluorescence.csv file

--- Test1.py
+++ Test1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import signal, stats
 import seaborn as sns
-
+from pathlib import Path
 # params.py
 import os
 FLUORESCENCE_PATH = "Fluorescence.csv"  # Update full path if needed
@@ -30,7 +30,7 @@ print(df_clean['Events'].value_counts(dropna=False))
 mapping = {
     '0': 0,
     'Input1*2*0': 1,  # e.g. cue
-    'Input1*2*1': 2,  # e.g. reward
+    'Input1*2*1': 2,  # e.g. cue2
 }
 
 df_clean['Events'] = df_clean['Events'].astype(str).map(mapping).fillna(0).astype(int)
@@ -40,3 +40,14 @@ print(df_clean['Events'].value_counts(dropna=False))
 "df.set_index('TimeStamp').resample('50ms').mean().reset_index()"
 
 "need to save in a new file"
+#output_path = (r"/Users/Lou/Desktop")
+#Path("results").mkdir(exist_ok=True)  # Folder if missing
+
+df_save = df_clean[['TimeStamp', 'Events', 'CH1-410', 'CH1-470', 'CH1-560']].round(4)  # 4 decimals
+df_save.to_csv(r"/Users/Lou/Desktop/cleaned_fluorescence.csv", index=False, float_format='%.4f')
+print("file saved")
+
+# LOAD BACK TEST
+df_loaded = pd.read_csv(r"/Users/Lou/Desktop/cleaned_fluorescence.csv")
+assert df_loaded.shape == df_clean.shape, "Load mismatch!"
+print("Reload success:", df_loaded.head())
